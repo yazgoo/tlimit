a146bdb436c855ecf842ce1c4de62097691c4b74
Olivier Abdesselam <yazgoo@users.noreply.github.com>
2018-05-30 14:20:18 +0200

    fix limit size

--- tlimit.js
+++ tlimit.js
@@ -2,6 +2,6 @@ if(window.tlimit_count == undefined)
   window.tlimit_count = 10
 browser.tabs.onCreated.addListener(t=> {
   browser.tabs.query({currentWindow: true}).then( tabs => {
-    if(tabs.length >= window.tlimit_count) browser.tabs.remove(t.id)
+    if(tabs.length > window.tlimit_count) browser.tabs.remove(t.id)
   })
 })
