6909f7f2cc931eb79051b8f1ccb9884f3ca6badd
yazgoo <yazgoo@gmail.com>
2018-05-30 20:16:25 +0200

    Add text field to edit value

--- popup/tlimit.js
+++ popup/tlimit.js
@@ -1,6 +1,17 @@
 browser.runtime.getBackgroundPage().then(win => {
   function showVal() {
-    document.getElementById("val").innerHTML = "" + win.tlimit_count
+    var val = document.getElementById("val")
+    val.value = "" + win.tlimit_count
+    val.size = val.value.length
+  }
+  function updateVal() {
+    var val = document.getElementById("val")
+    var value = parseInt(val.value)
+    if (isNaN(value)) {
+      showVal()
+    } else {
+      win.tlimit_count = value
+    }
   }
   function limit(increment) {
     if ((win.tlimit_count + increment) > 1)
@@ -9,5 +20,6 @@ browser.runtime.getBackgroundPage().then(win => {
   }
   document.getElementById("minus").addEventListener("click", e => limit(-1))
   document.getElementById("plus").addEventListener("click", e => limit(1))
+  document.getElementById("val").addEventListener("change", e => updateVal())
   showVal()
 })
