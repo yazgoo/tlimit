758c5e4819d06daf9073e8ee74de8ca21975f3b3
yazgoo <yazgoo@gmail.com>
2018-05-30 13:45:50 +0200

    Support intermediate tabs fix #1

--- tlimit.js
+++ tlimit.js
@@ -1,5 +1,7 @@
 if(window.tlimit_count == undefined)
   window.tlimit_count = 10
 browser.tabs.onCreated.addListener(t=> {
-  if(t.index > window.tlimit_count) browser.tabs.remove(t.id)
-} )
+  browser.tabs.query({currentWindow: true}).then( tabs => {
+    if(tabs.length >= window.tlimit_count) browser.tabs.remove(t.id)
+  })
+})
